b7fa0f976908a1124fee977f11d47b3181a6aa2f
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-04-01 13:47:48 -0400

    added svgs for cursor icon, store for current color, circles instead of box fill-in

--- src/components/number-chart/Cell.svelte
+++ src/components/number-chart/Cell.svelte
@@ -1,21 +1,38 @@
 <script>
   let color = "white";
+  import { penColor } from "./store";
   const handleClick = () => {
     // TODO: need to make a bar for selecting colors and erases.
-    //Will use a svelte store for holding the color selection
-    color = "black";
+    color = $penColor;
+  };
+
+  export let h, w;
+  let diameter;
+  // we need to wrap this as reactive, as `h` and `w` start as undefined
+  $: {
+    if (h < w) {
+      diameter = h / 14;
+    } else {
+      diameter = w / 14;
+    }
   }
 </script>
-
 
 <style>
   td {
     border-color: black;
     border-style: solid;
     border-width: 2px;
-    cursor: pointer;
-    user-select: none;
+  }
+  span {
+    border-radius: 50%;
+    display: inline-block;
   }
 </style>
 
-<td width="10%" style="background-color: {color};" on:click={handleClick}>&nbsp;</td>
+<td width="10%" on:click={handleClick}>
+  <span
+    style="background-color: {color}; height: {diameter}px; width: {diameter}px;">
+    &nbsp;
+  </span>
+</td>
--- src/components/number-chart/Table.svelte
+++ src/components/number-chart/Table.svelte
@@ -3,6 +3,19 @@
   export let color = "black";
 
   import Cell from "./Cell.svelte";
+
+  let h, w;
+
+  import { penColor } from "./store";
+
+  let url = "";
+  if ($penColor === "black") {
+    url = "/icons/edit-black.svg";
+  } else if ($penColor === "blue") {
+    url = "/icons/edit-blue.svg";
+  } else if ($penColor === "red") {
+    url = "/icons/edit-red.svg";
+  }
 </script>
 
 <style>
@@ -10,25 +23,27 @@
     border-collapse: collapse;
     display: inline-table;
     width: 20%;
+    text-align: center;
+    user-select: none;
   }
   caption {
     border-color: black;
     border-style: solid;
     border-width: 2px;
-    cursor: pointer;
-  }
-  caption {
     border-bottom-width: 1px;
   }
 </style>
 
 <table>
   <caption style="color: {color}">{header}</caption>
-  <tbody>
+  <tbody
+    style="cursor: url({url}) 0 32, pointer;"
+    bind:clientWidth={w}
+    bind:clientHeight={h}>
     {#each Array(10) as i}
       <tr>
         {#each Array(10) as j}
-          <Cell />
+          <Cell {h} {w} />
         {/each}
       </tr>
     {/each}
