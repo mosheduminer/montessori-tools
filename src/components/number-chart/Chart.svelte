3bf29c37acc0730683084fec1bc2a6f5269c9185
Menachem M. Hornbacher <me@mendel.tech>
2020-04-02 09:17:06 -0400

    update chart component

--- src/components/number-chart/Chart.svelte
+++ src/components/number-chart/Chart.svelte
@@ -1,12 +1,18 @@
 <script>
-  let headers = ["10,000", "1,000", "100", "10", "1"];
-  let colors = ["blue", "red", "green", "black", "blue"];
   import Table from "./Table.svelte";
   import PenBar from "./PenBar.svelte";
+
+  let headers = ["10,000", "1,000", "100", "10", "1"];
+  let colors = ["blue", "red", "green", "black", "blue"];
+
+  let chart;
 </script>
 
 <style>
-  div {
+  #number-chart {
+    background: #fff;
+  }
+  #number-chart > div {
     display: flex;
   }
   input {
@@ -17,14 +23,16 @@
   }
 </style>
 
-<PenBar />
+<PenBar chart={chart} />
 
-{#each headers as header, index}
-  <Table {header} color={colors[index]} />
-{/each}
-
-<div>
-  {#each Array(5) as i}
-    <input type="number" min="0" max="9" />
+<div id="number-chart" bind:this={chart}>
+  {#each headers as header, index}
+    <Table {header} color={colors[index]} />
   {/each}
+
+  <div>
+    {#each Array(5) as i}
+      <input type="number" min="0" max="9" />
+    {/each}
+  </div>
 </div>
