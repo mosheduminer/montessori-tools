28c84a59de4e49aefec5eef9ec2f7b9ea24d83dc
Arye Eidelman <aryedov.e@gmail.com>
2020-05-22 14:25:52 -0400

    fix a rounding error that can couse two letters/pegs to overlap in the drawer
    If for example you drag the blue peg onto the red peg in the drawer
    it can sometimes stay there.
    this is caused by the x or y being a non round number like 77.99999999
    the same issue applies to the letters.

--- src/lib/normalizer.js
+++ src/lib/normalizer.js
@@ -3,21 +3,21 @@
  */
 
 export function normalizePegCoords(x, y) {
-    let modX = (x - 14) % 64;
-    let modY = (y - 14) % 64;
-
     return {
-        x: modX < 32 ? x - modX : x + 64 - modX,
-        y: modY < 32 ? y - modY : y + 64 - modY
+        x: round(x, { offset: 14, step: 64 }),
+        y: round(y, { offset: 14, step: 64 }),
     };
 };
 
 export function normalizeLetterCoords(x, y) {
-    let modX = (x - 14) % 64;
-    let modY = (y - 14) % 80;
-
     return {
-        x: modX < 32 ? x - modX : x + 64 - modX,
-        y: modY < 40 ? y - modY : y + 80 - modY
+        x: round(x, { offset: 14, step: 64 }),
+        y: round(y, { offset: 14, step: 80 }),
     };
+};
+
+function round(value, { offset, step }) {
+    offset || (offset = 0);
+    step || (step = 1);
+    return Math.round((value - offset) / step) * step + offset;
 };
