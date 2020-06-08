67c19c42feb91dcbe4d55fadf5ef68ec00d749fb
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-06-08 07:10:37 -0400

    minor fixes
    change order of cards in golden beads, enforce lower magnitude cards always above greater magnitude number cards even when dragging. change draggable to listen for end events on svelte:window, eliminating some bugs

--- src/components/golden-beads/Cards.svelte
+++ src/components/golden-beads/Cards.svelte
@@ -3,7 +3,7 @@
   import SpawnArea from "../library/SpawnArea.svelte";
 
   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let magnitudes = [1, 10, 100, 1000];
+  let magnitudes = [1000, 100, 10, 1];
   let colors = ['green', 'blue', 'red', 'green']
 </script>
 
@@ -13,7 +13,7 @@
     border: 4px solid #895f2b;
     background-color: burlywood;
     padding: 5px;
-    grid-template-columns: 24px calc(24px + 1rem) calc(24px + 2rem) calc(24px + 3rem);
+    grid-template-columns: calc(24px + 3rem) calc(24px + 2rem) calc(24px + 1rem) 24px;
     grid-gap: 5px;
   }
 </style>
@@ -24,14 +24,14 @@
       <SpawnArea
         on:spawn
         component={NumberCard}
-        data={{ style: `color: ${colors[index]}; width: calc(24px + ${index}rem); padding-left: 0; padding-right: 0; z-index: ${500 - index}`, 
+        data={{ style: `color: ${colors[index]}; width: calc(24px + ${3 - index}rem); padding-left: 0; padding-right: 0; z-index: ${10000 - (3 - index * 500)}`, 
           number: number * magnitude }}>
         <NumberCard
           disabled={true}
           x={index === 3 ? 136 : index === 2 ? 80 : index === 1 ? 40 : 16}
           y={columnIndex * 44 + 16}
           number={number * magnitude}
-          style={`color: ${colors[index]}; width: calc(24px + ${index}rem); padding-left: 0; padding-right: 0; z-index: ${500 - index}`} />
+          style={`color: ${colors[index]}; width: calc(24px + ${3 - index}rem); padding-left: 0; padding-right: 0; z-index: ${10000 - (3 - index * 500)}`} />
       </SpawnArea>
     {/each}
   {/each}
