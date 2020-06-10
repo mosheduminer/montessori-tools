63403e87eee21f168e5280ef96c97f25b65975fc
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-06-10 12:40:46 -0400

    add rest-spread for babel

--- rollup.config.js
+++ rollup.config.js
@@ -43,6 +43,7 @@ export default {
 					}]
 				],
 				plugins: [
+					'@babel/plugin-proposal-object-rest-spread',
 					'@babel/plugin-syntax-dynamic-import',
 					['@babel/plugin-transform-runtime', {
 						useESModules: true
