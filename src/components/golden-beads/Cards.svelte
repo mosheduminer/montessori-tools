a37bcbe13cf86ddb5fef59ac257bc8c740987e71
Menachem M. Hornbacher <menachem@rallybound.com>
2020-06-30 12:19:09 -0400

    fix color order

--- src/components/golden-beads/Cards.svelte
+++ src/components/golden-beads/Cards.svelte
@@ -4,7 +4,7 @@
 
   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let magnitudes = [1000, 100, 10, 1];
-  let colors = ['green', 'blue', 'red', 'green']
+  let colors = ['green', 'red', 'blue', 'green']
 </script>
 
 <style>
